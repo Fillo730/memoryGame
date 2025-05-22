a99945cccf9047463f66a2b9dbd5d8386e78dd7f
Fillo730 <filloinformatica@gmail.com>
2025-05-22 13:06:38 +0200

    feat: improved nature gallery in the homepage

---
+++ src/home/natureGallery/NatureGallery.jsx
@@ -0,0 +1,42 @@
+//Libraries
+import { useState } from 'react';
+import _ from 'lodash';
+
+//Components
+import { CustomButton } from '../../components/ComponentsDependencies';
+
+//Objects
+import defaultImages from '../../../public/DefaultImages';
+
+//CSSFiles
+import './NatureGallery.css';
+
+function NatureGallery() {
+    const [randomImages, setRandomImages] = useState(
+        getUniqueRandomImage());
+
+    function getUniqueRandomImage() {
+        const shuffledImages = _.shuffle(defaultImages);
+        return shuffledImages.slice(0, 6);
+    }
+
+    function handleClick() {    
+        const newImages = getUniqueRandomImage();
+        setRandomImages(newImages);
+    }
+
+    return (
+        <>
+            <div className="gallery-container">
+                {
+                    randomImages.map((src, index) => (
+                        <img key={index} className="gallery-item" src={src} alt={`Nature ${index + 1}`} />
+                    ))
+                }
+            </div>
+            <CustomButton text={"See more"} handleClick={handleClick} />
+        </>
+  )
+}
+
+export default NatureGallery;
